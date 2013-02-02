d9726ec9934f138f9856ffe13189a47491038e98
Eric Rochester <erochest@virginia.edu>
2013-02-01 21:19:24 -0500

    Working through getting TLS+SMTP working. Not there yet.

--- Network/Mail/SMTP/TLS.hs
+++ Network/Mail/SMTP/TLS.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 -- | This is a fairly simple module that just connects the smtp-mail package
 -- with the tls package. Hopefully this will make it easier to connect to, say,
@@ -13,9 +14,16 @@ module Network.Mail.SMTP.TLS
     , tlsParams
     ) where
 
-import           Control.Monad (unless)
+import           Control.Applicative
+import           Control.Monad (unless, forM_)
 import qualified Crypto.Random.AESCtr as RNG
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Base64 as B64
+import           Data.ByteString.Lazy (fromChunks)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
 import           Data.List (isPrefixOf)
+import           Data.Monoid
 import           Network
 import           Network.Mail.Mime
 import           Network.Mail.SMTP
@@ -23,6 +31,7 @@ import           Network.Socket
 import           Network.TLS
 import           Network.TLS.Extra
 import           System.IO
+import           Text.Printf
 
 ciphers :: [Cipher]
 ciphers = [ cipher_AES128_SHA1
@@ -34,27 +43,113 @@ ciphers = [ cipher_AES128_SHA1
 tlsParams :: TLSParams
 tlsParams = defaultParamsClient { pCiphers = ciphers }
 
+write :: Handle -> String -> IO ()
+write h cmd = do
+        hPrintf h "%s\r\n" cmd
+        printf ">>> %s\n" cmd
+
+writebs :: Handle -> B.ByteString -> IO ()
+writebs h cmd = do
+        B.hPut h cmd
+        hPutStr h "\r\n"
+        printf ">>> %s\n" . T.unpack $ TE.decodeUtf8 cmd
+
 waitFor :: Handle -> String -> IO ()
 waitFor h str = do
         ln <- hGetLine h
+        putStrLn $ "<<< " <> ln
         unless (str `isPrefixOf` ln) (waitFor h str)
 
-sendMailTls :: HostName -> UserName -> Password -> Mail -> IO ()
-sendMailTls host = sendMailTls' host (PortNum 587)
+tlsWrite :: Context -> String -> IO ()
+tlsWrite ctx cmd = do
+        sendData ctx . fromChunks
+                     . (:[])
+                     . TE.encodeUtf8
+                     . T.pack
+                     . printf "%s\r\n"
+                     $ cmd
+        printf ">>> %s\n" cmd
 
+tlsWritebs :: Context -> B.ByteString -> IO ()
+tlsWritebs ctx cmd = do
+        sendData ctx $ fromChunks [cmd, "\r\n"]
+        printf ">>> %s\n" . T.unpack $ TE.decodeUtf8 cmd
+
+tlsWaitFor :: Context -> String -> IO ()
+tlsWaitFor ctx str = do
+        -- [ln] <- take 1
+        --       . filter (T.isPrefixOf (T.pack str))
+        lns <- takeWhile (not . T.isPrefixOf (T.pack str))
+              . T.lines
+              . TE.decodeUtf8
+            <$> recvData ctx
+        -- printf . T.unpack $ "<<< " <> ln
+        forM_ lns $ printf . T.unpack . ("<<< " <>)
+
+sendMailTls :: HostName -> UserName -> Password -> Mail -> IO ()
+sendMailTls host = sendMailTls' host 587
+
+b64 :: String -> B.ByteString
+b64 = B64.encode . B.pack . map (toEnum . fromEnum)
+
+-- TODO: option to pass in TLSParams
 -- TODO: run in EitherT.
-sendMailTls' :: HostName -> PortNumber -> UserName -> Password -> Mail -> IO ()
+
+sendMailTls' :: HostName -> Int -> UserName -> Password -> Mail -> IO ()
 sendMailTls' host port user passwd mail = do
+        g <- RNG.makeSystem
+        let pn = PortNumber $ fromIntegral port
+        h <- connectTo host pn
+        hSetBuffering h LineBuffering
+
+        write h "EHLO"
+        waitFor h "250-STARTTLS"
+        write h "STARTTLS"
+        waitFor h "220"
+
+        ctx <- contextNewOnHandle h tlsParams g
+        putStrLn "handshake"
+        handshake ctx
+
+        putStrLn "login"
+        tlsWrite ctx "EHLO"
+        tlsWaitFor ctx "250"
+        tlsWrite ctx "AUTH LOGIN"
+        tlsWritebs ctx $ b64 user
+        tlsWritebs ctx $ b64 passwd
+        tlsWaitFor ctx "220"
+
+        -- _ <- login smtp user passwd
+        -- putStrLn "renderAndSend"
+        -- renderAndSend smtp mail
+        -- putStrLn "closeSMTP"
+        -- closeSMTP smtp
+
+        putStrLn "bye"
+        bye ctx
+
+{-
         g    <- RNG.makeSystem
+        putStrLn "connectSMTP'"
         smtp <- connectSMTP' host port
         let h = smtpHandle smtp
+
+        putStrLn "waitFor 250"
         waitFor h "250-STARTTLS"
         hPrint h "STARTTLS\r\n"
+        putStrLn "waitFor 220"
         waitFor h "220"
+        putStrLn "contextNewOnHandle"
         con <- contextNewOnHandle h tlsParams g
+        putStrLn "handshake"
         handshake con
+        putStrLn "login"
         _ <- login smtp user passwd
+        putStrLn "renderAndSend"
         renderAndSend smtp mail
+        putStrLn "closeSMTP"
         closeSMTP smtp
+        putStrLn "bye"
         bye con
+        -}
 
--- test/TlsSmtpTest.hs
+++ test/TlsSmtpTest.hs
@@ -1,16 +1,109 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+{-
+import Network.Socket
+import Network
+import Network.TLS
+import Network.TLS.Extra
+
+import Crypto.Random
+import qualified Crypto.Random.AESCtr as RNG
+import System.IO
+import Text.Printf
+
+import Data.Monoid
+import Control.Monad (when)
+import Data.List (isPrefixOf)
+
+ciphers :: [Cipher]
+ciphers =
+        [ cipher_AES128_SHA1
+        , cipher_AES256_SHA1
+        , cipher_RC4_128_MD5
+        , cipher_RC4_128_SHA1
+        ]
+
+main :: IO ()
+main = emailGmail2
+
+cWrite :: Handle -> String -> IO ()
+cWrite h s  = do
+    hPrintf h "%s\r\n" s
+    printf    "> %s\n" s
+
+cWaitFor :: Handle -> String -> IO ()
+cWaitFor h str = do
+    ln <- hGetLine h
+    putStrLn ln
+    when (not $ str `isPrefixOf` ln) (cWaitFor h str)
+
+emailGmail2 = do
+    let
+        host = "smtp.gmail.com"
+        port = 587
+        params = defaultParamsClient{pCiphers = ciphers}
+        p = PortNumber (fromIntegral port)
+    putStrLn $ "port = " <> show p
+    g <- RNG.makeSystem
+    h <- connectTo host p
+    hSetBuffering h LineBuffering
+    cWrite h "EHLO"
+    cWaitFor h "250-STARTTLS"
+    cWrite h "STARTTLS"
+    cWaitFor h "220"
+    con <- contextNewOnHandle h params g
+    handshake con
+    bye con
+-}
 
 import Control.Applicative
 import Data.Maybe (maybe)
+import Data.Monoid
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TLE
+import Data.Time
+import Data.Word
+import Network.Mail.Mime
+import Network.Mail.SMTP.TLS
+import Network (PortID(..))
 import Network.Socket (PortNumber(..))
 import System.Environment (getEnv, getEnvironment)
 
+
+from    = Address Nothing "erochest@gmail.com"
+to      = Address Nothing "eric@ericrochester.com"
+subject = "Test Email"
+body    = "email body" :: TL.Text
+html    = "<h1>HTML</h1>\n<p>Hi-ya!</p>" :: TL.Text
+
+-- From smtp-mail:
+
+-- | Construct a plain text 'Part'
+plainTextPart :: TL.Text -> Part
+plainTextPart = Part "text/plain; charset=utf-8" 
+                     QuotedPrintableText Nothing [] . TLE.encodeUtf8
+
+-- | Construct an html 'Part'
+htmlPart :: TL.Text -> Part
+htmlPart = Part "text/html; charset=utf-8" 
+                QuotedPrintableText Nothing [] . TLE.encodeUtf8
 
 main :: IO ()
 main = do
         env  <- getEnvironment
         host <- getEnv "SMTP_HOST"
-        let port = PortNum . maybe 587 read $ lookup "SMTP_PORT" env
-        putStrLn host
-        print port
+        -- let port = PortNumber . maybe 587 read $ lookup "SMTP_PORT" env
+        let port = 587
+
+        user <- getEnv "SMTP_USER"
+        pass <- getEnv "SMTP_PASS"
+        now  <- getCurrentTime
+        mail <- simpleMail from to subject
+                           (body <> TL.pack (show now))
+                           (html <> "<p>" <> TL.pack (show now) <> "</p>")
+                           []
+        sendMailTls' host port user pass mail
+        putStrLn "sent."
+
